cd0f8c47fa8b3e5dc4e487c65a639789b9a9fdc2
Sinoyolo Mabuza <Mabuza834@gmail.com>
2025-07-01 16:37:27 +0200

    Initial commit

---
+++ src/test/java/com/sinoyolo/smartservicerequestsystem/SmartServiceRequestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinoyolo.smartservicerequestsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartServiceRequestSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
